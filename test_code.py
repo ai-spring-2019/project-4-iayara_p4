401c6997e239587ab6949398b06e7e9fb407c8ba
Ines Ayara <iayara@hamilton.edu>
2019-05-13 23:21:19 -0400

    code cleaned up

--- test_code.py
+++ test_code.py
@@ -1,12 +1,7 @@
 """
 Name: Ines Ayara
 Project 4: Classification with Neural Networks.
-How to use this program: To collect accuracy data from running the NN on a data
-set, follow these instructions:
-    1. In main, set input and output sizes where specified.
-    2. Run with: python3 project4.py <my_data_set>.csv
-It should produce the accuracy for a range of network strcuture, along with the
-number of layers and nodes per layer that were used.
+How to use this program:
 """
 import csv, sys, random, math
 from sklearn import preprocessing
@@ -65,6 +60,13 @@ def logistic_vect(v1):
         v2.append(logistic(val))
     return v2
 
+def print_matrix(m):
+    for r in m:
+        row = ''
+        for c in r:
+            row += str(c) + ' '
+        print(row)
+
 def accuracy(nn, pairs):
     """Computes the accuracy of a network on given pairs. Assumes nn has a
     predict_class method, which gives the predicted class for the last run
@@ -83,12 +85,9 @@ def accuracy(nn, pairs):
     for (x, y) in pairs:
         nn.forward_propagate(x)
         class_prediction = nn.predict_class()
-        # For bit-incrementer:
-        if class_prediction != y:
-        # If dealing with a non-classification problem, uncomment this:
+        #if class_prediction != y:
         #if class_prediction != y[0]:
-        # If dealing with a classification problem, uncomment this:
-        #if class_prediction != y.index(max(y)):
+        if class_prediction != y.index(max(y)):
             true_positives += 1
 
         outputs = nn.get_outputs()
@@ -100,7 +99,6 @@ def accuracy(nn, pairs):
 ### Neural Network code goes here
 class NeuralNetwork:
     def __init__(self, nn_dims):
-
         self.layers = nn_dims
         self.total_layers = len(nn_dims)
         self.input_size = nn_dims[0]
@@ -134,12 +132,9 @@ class NeuralNetwork:
         current_layer = self.hidden_layers[layer_num]
         w_matrix = self.get_matrix_of_weights(current_layer,
                                               self.input_size + 1)
-        # Set weights for the links between the input layer and the first
-        # hidden layer.
         self.link_weights.append(w_matrix)
 
         layer_num += 1
-        # Set weights of the links between consecutive hidden layers.
         while layer_num < self.num_hidden_layers:
             prev_layer = current_layer
             current_layer = self.hidden_layers[layer_num]
@@ -148,13 +143,15 @@ class NeuralNetwork:
                                                                 prev_layer + 1))
             layer_num += 1
 
-        # Set weights for the links between the last hidden layer and the
-        # output layer.
         self.link_weights.append(self.get_matrix_of_weights(self.output_size,
                                                            current_layer + 1))
 
     def forward_propagate(self, x):
-        """ Propagate the inputs forward to compute the outputs."""
+        """ Given an input vector and the matrix of all weights, updates:
+            - self.output = output value
+            - self.inputs = outputs before activation
+            - self.outputs = outputs after activation
+        """
 
         self.outputs = [x]
         self.inputs = [x]
@@ -167,15 +164,14 @@ class NeuralNetwork:
                     sum += self.output[node] * row[node]
                 z.append(sum)
             self.inputs.append(z)
-            self.output = logistic_vect(z) + [1] # Add dummy node
+            #self.outputs.append(logistic_vect(z))
+            # Add dummy node
+            self.output = logistic_vect(z) + [1]
             self.outputs.append(self.output)
 
-        # We don't want to have a dummy value (1) with our final output.
         self.output = self.output[:-1]
 
     def back_propagation_learning(self, training):
-        """ Propagate deltas backward from the output layer to input layer."""
-
         alpha = 1
         t = 0
         self.set_network_random_weights()
@@ -183,14 +179,15 @@ class NeuralNetwork:
             for (x, y) in training:
                 self.forward_propagate(x)
                 self.deltas = []
-                # Calculate the error vect delta from the output layer.
+
+                # Propagate deltas backward from output layer to input layer
                 delta = [0 for _ in range(len(self.output))]
                 for j in range(len(self.output)):
                     deriv_sigmoid = logistic(self.inputs[-1][j]) * (1 - logistic(self.inputs[-1][j]))
                     delta[j] = deriv_sigmoid * (y[j] - self.output[j])
 
-                # Propagate deltas from last hidden layer to input layer.
                 self.deltas.append(delta)
+
                 for l in range(self.total_layers - 2, -1, -1):
                     delta = [0 for _ in range(len(self.outputs[l]))]
                     for i in range(len(self.inputs[l])):
@@ -213,14 +210,9 @@ class NeuralNetwork:
             t += 1
 
     def predict_class(self):
-        """ Returns the predicted value, whether it's a class or a rounded
-            output value. """
-        # If dealing with a non-classification problem, uncomment this:
-        return float(round(self.output[0]))
-        # If dealing with a classification problem, uncomment this:
-        #return self.output.index(max(self.output))
-        # If testing the 3-bit-increment:
-        #return [float(round(x)) for x in self.output]
+        #return [float(round(out)) for out in self.output]
+        #return float(round(self.output[0]))
+        return self.output.index(max(self.output))
 
     def get_outputs(self):
         return self.output
@@ -228,6 +220,7 @@ class NeuralNetwork:
 def normalize_wine_dataset():
     # Get dataset
     df = pd.read_csv("wine.csv", sep=",")
+
     cols = []
     rows = [[]]
 
@@ -261,15 +254,15 @@ def normalize_wine_dataset():
 
 def main():
     header, data = read_data(sys.argv[1], ",")
+
     pairs = convert_data_to_pairs(data, header)
 
     # Note: add 1.0 to the front of each x vector to account for the dummy input
     training = [([1.0] + x, y) for (x, y) in pairs]
+    random.shuffle(training)
 
-    # Uncomment only if running the wine data (normalized_wine_data.csv).
-    #random.shuffle(training)
 
-    # K-fold Cross Validation: (not for 3-bit-increment)
+    # K-fold Cross Validation:
     k = 5
     offset = len(training) // k
     data = [training[x:x+offset] for x in range(0, len(training), offset)]
@@ -277,10 +270,10 @@ def main():
     training_sets = data[:-1]
     validation_set = data[-1]
 
-    # Data collection Script:
-    # in = 2, out = 1 should work for generated and banana.
-    input = [2]  # Set length of input.
-    output = [1] # Set length of output.
+    # Data collection Script
+    input = [13]
+    output = [3]
+    file = "Wine:"
     for node in range(1, 11):
         for layer in range(1, 4):
             dims = input + [node * layer] + output
@@ -291,9 +284,27 @@ def main():
                 avg_accuracy += accuracy(nn, validation_set)
             avg_accuracy = avg_accuracy / len(training_sets)
 
+            print(file)
             print("Layers:", layer, "Nodes:", node)
             print("Accuracy:", avg_accuracy)
             print("**********")
+
+    # Generated: classification
+    #nn = NeuralNetwork([2, 3, 1])
+    # Wine: non-classification
+    #random.shuffle(training)
+    #nn = NeuralNetwork([13, 4, 3])
+    # banana: classification
+    #nn = NeuralNetwork([2, 2, 1])
+    # bits: non-classification
+    #nn = NeuralNetwork([3, 6, 3])
+    # breast cancer: classification
+    #nn = NeuralNetwork([30, 15, 1]) #weird
+
+    #nn.back_propagation_learning(training, test_pairs)
+
+    #print(accuracy(nn, pairs))
+    #print(accuracy(nn, test_pairs))
 
 if __name__ == "__main__":
     main()
